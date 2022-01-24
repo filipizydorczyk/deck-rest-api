37ef9c7a94ce42040ec25a9933bd020aa32e7d1a
Filip Izydorczyk <filip.izydorczyk@protonmail.com>
2022-01-24 19:44:55 +0100

    dev: poc checking if deck app is installed

--- lib/Controller/BoardApiController.php
+++ lib/Controller/BoardApiController.php
@@ -2,15 +2,19 @@
 
 namespace OCA\DeckREST\Controller;
 
-use \OCP\AppFramework\ApiController;
-use \OCP\IRequest;
+use OCP\AppFramework\ApiController;
+use OCP\AppFramework\Http;
+use OCP\AppFramework\Http\DataResponse;
+use OCP\IRequest;
 
 class BoardApiController extends ApiController
 {
+    private \OCP\App\IAppManager $appManager;
 
-    public function __construct($appName, IRequest $request)
+    public function __construct($appName, IRequest $request, \OCP\App\IAppManager $appManager)
     {
         parent::__construct($appName, $request);
+        $this->appManager = $appManager;
     }
 
     /**
@@ -20,6 +24,9 @@ class BoardApiController extends ApiController
     public function index()
     {
         // @CORS
-        return "Hello World";
+        if (!$this->appManager->isEnabledForUser("deck")) {
+            return new DataResponse("Deck app is required to be installed for this API to work", HTTP::STATUS_FAILED_DEPENDENCY);
+        }
+        return new DataResponse("Hello World", HTTP::STATUS_OK);
     }
 }
