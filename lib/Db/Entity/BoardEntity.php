8f841a1bfe23a2e8702b9944129358414cfb6f38
Filip Izydorczyk <filip.izydorczyk@protonmail.com>
2022-02-02 00:07:59 +0100

    ref: some board type solitions

--- lib/Db/Entity/BoardEntity.php
+++ lib/Db/Entity/BoardEntity.php
@@ -3,23 +3,30 @@
 namespace OCA\DeckREST\Db\Entity;
 
 use OCP\AppFramework\Db\Entity;
+use OCP\IUser;
 
 class BoardEntity extends Entity implements \JsonSerializable
 {
     protected $title;
     protected $owner;
     protected $color;
-    protected $archived = false;
+    protected bool $archived = FALSE;
     /** @var Label[]|null */
-    protected $labels = null;
+    protected $labels = [];
     /** @var Acl[]|null */
-    protected $acl = null;
+    protected $acl = [];
     protected $permissions = [];
     protected $users = [];
-    protected $shared;
+    protected $shared = 0;
     protected $stacks = [];
     protected $deletedAt = 0;
     protected $lastModified = 0;
+
+    // public function __construct(IUser $user)
+    // {
+    //     $this->owner = $user;
+    //     $this->getter('owner');
+    // }
 
     protected $settings = [];
 
@@ -27,7 +34,12 @@ class BoardEntity extends Entity implements \JsonSerializable
     {
         return [
             'title' => $this->title,
-            'owner' => $this->owner,
+            'owner' =>  $this->owner,
+            // [
+            //     'uid' => $this->owner->getUID(),
+            //     'displayname' => $this->owner->getDisplayName(),
+            //     'type' => 0
+            // ],
             'color' => $this->color,
             'archived' => $this->archived,
             'labels' => $this->labels,
