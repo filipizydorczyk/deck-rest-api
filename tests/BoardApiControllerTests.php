1b60787920aea7270bd40c15a7c1e7ebc83e7cca
Filip Izydorczyk <filip.izydorczyk@protonmail.com>
2022-02-06 02:23:16 +0100

    dev: added template for api request in tests

--- tests/BoardApiControllerTests.php
+++ tests/BoardApiControllerTests.php
@@ -2,12 +2,20 @@
 
 declare(strict_types=1);
 
+use GuzzleHttp\Client;
 use PHPUnit\Framework\TestCase;
 
 final class BoardApiControllerTests extends TestCase
 {
     public function testBasicTestCase(): void
     {
-        $this->assertEquals(true, true);
+        $client = new Client([
+            'base_uri' => 'http://localhost:4325',
+            'auth' => ['test', 'test'],
+        ]);
+
+        $response = $client->request('GET', '/apps/deckrestapi/api/v1/boards');
+
+        $this->assertEquals(200, $response->getStatusCode());
     }
 }
