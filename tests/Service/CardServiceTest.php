cbdf6b0db929bcf53a1c2ba9e31527c04ae62fd5
Filip Izydorczyk <filip.izydorczyk@protonmail.com>
2022-02-21 01:34:17 +0100

    dev: test for find all card service

--- tests/Service/CardServiceTest.php
+++ tests/Service/CardServiceTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace OCA\DeckREST\Tests;
 
+use OCA\DeckREST\Db\Entity\CardEntity;
 use OCA\DeckREST\Db\Mapper\CardMapper;
 use OCA\DeckREST\Service\CardService;
 use OCA\DeckREST\Service\LabelService;
@@ -26,5 +27,18 @@ final class CardServiceTest extends TestCase
         $this->labelService = $this->createMock(LabelService::class);
         $this->cardMapper = $this->createMock(CardMapper::class);
         $this->cardService = new CardService($this->cardMapper, $this->labelService);
+    }
+
+    public function testFindAll()
+    {
+        $card = new CardEntity();
+        $card->setId(1);
+
+        $this->cardMapper->expects($this->any())->method('findAll')->willReturn([$card]);
+
+        $this->assertEquals(
+            count($this->cardService->findAll()),
+            1
+        );
     }
 }
--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,6 +4,7 @@ require_once __DIR__ . '/../nextcloud/lib/composer/autoload.php';
 require_once __DIR__ . '/../lib/Db/Mapper/BoardMapper.php';
 require_once __DIR__ . '/../lib/Db/Mapper/StackMapper.php';
 require_once __DIR__ . '/../lib/Db/Mapper/LabelMapper.php';
+require_once __DIR__ . '/../lib/Db/Mapper/CardMapper.php';
 require_once __DIR__ . '/../lib/Service/LabelService.php';
 require_once __DIR__ . '/../lib/Service/StackService.php';
 require_once __DIR__ . '/../lib/Service/BoardService.php';
